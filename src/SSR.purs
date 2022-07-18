f470a3522a35bf7eb9c267b379567194b217b7f7
Verity Scheel <monoidmusician@gmail.com>
2022-07-18 11:27:54 -0400

    SSRn't

--- src/SSR.purs
+++ src/SSR.purs
@@ -1,22 +1,28 @@
 module SSR where
 
-
+import Prelude (($), pure)
+import Data.String as String
 import Deku.Toplevel (Template(..), runSSR)
 import Effect (Effect)
 import Parser.Main as Parser
 
 ssr :: Effect String
-ssr = runSSR
-    ( Template
-        { head:
-            """<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8" />
+ssr = pure $ String.trim
+    """
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width">
-		<title>PureScript Blog</title>
-	</head>"""
-        , tail: "</html>"
-        }
-    )
-    Parser.main
+    <title>PureScript Blog</title>
+    </head>
+    <body>
+      <div data-widget="Widget.Query" data-widget-datakey="default" data-widget-data-keys="grammar"></div>
+      <div data-widget="Parser.Main" data-widget-datakey="default"></div>
+      <div data-widget="Parser.Input" data-widget-datakey="default"></div>
+      <div data-widget="Parser.Random" data-widget-datakey="default"></div>
+      <div data-widget="Parser.Explorer" data-widget-datakey="default"></div>
+      <div data-widget="Parser.StateTable" data-widget-datakey="default"></div>
+      <div data-widget="Parser.ParseTable" data-widget-datakey="default"></div>
+    </body>
+    """
